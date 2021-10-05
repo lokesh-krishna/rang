c86e8ae6c27efbc93ab847dab5a6844f0493a1d0
Lokesh Krishna <lokesh@low-key.me>
2021-10-05 13:24:09 +0530

    Drop nerd font icon and notification icon border

--- rang.sh
+++ rang.sh
@@ -2,7 +2,7 @@
 
 grim -g "$(slurp -p)" - > /tmp/pixel.png
 magick /tmp/pixel.png -colors 1 txt: | tail -n 1 | awk '{print $3}' > /tmp/hex
-magick convert /tmp/pixel.png -filter point -resize 96x96 -bordercolor '#303030' -border 5 /tmp/big-pixel.png
-notify-send -i /tmp/big-pixel.png "  rang" "The hex code is $(cat /tmp/hex)"
+magick convert /tmp/pixel.png -filter point -resize 96x96 /tmp/big-pixel.png
+notify-send -i /tmp/big-pixel.png "rang" "The hex code is $(cat /tmp/hex)"
 wl-copy -n < /tmp/hex
 rm -f /tmp/pixel.png /tmp/big-pixel.png /tmp/hex
