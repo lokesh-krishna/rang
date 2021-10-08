b564fb7c91a780c075dfdc6d9da491dd5fa45590
Lokesh Krishna <lokesh@low-key.me>
2021-10-08 15:03:45 +0530

    Some refactoring

--- rang.sh
+++ rang.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+# variables
+bordercolor="#303030"
+border=5
+
 grim -g "$(slurp -p)" - > /tmp/pixel.png
-hexcode=$(magick /tmp/pixel.png -colors 1 txt: | tail -n 1 | awk '{print $3}')
-magick convert /tmp/pixel.png -filter point -resize 96x96 -bordercolor '#303030' -border 5 /tmp/big-pixel.png
+hexcode=$(magick /tmp/pixel.png -colors 1 txt: | awk 'NR==2 {print $3}')
+magick convert /tmp/pixel.png -filter point -resize 96x96 -bordercolor $bordercolor -border $border /tmp/big-pixel.png
 notify-send -i /tmp/big-pixel.png "  rang" "The hex code is $hexcode"
-echo $hexcode | wl-copy -n
+wl-copy -n $hexcode
 rm -f /tmp/pixel.png /tmp/big-pixel.png
