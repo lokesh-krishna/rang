8f6f94f1064af49301655e12d8cec78f45f5004c
Mohamad Supangat <hantamkoding@gmail.com>
2022-07-03 23:21:11 +0700

    fix error pick
    replace /tmp/pixcel.png to $image variable

--- rang.sh
+++ rang.sh
@@ -7,7 +7,7 @@ image="/tmp/image.png"
 icon="/tmp/icon.png"
 
 grim -g "$(slurp -p)" - > $image
-hexcode=$(magick /tmp/pixel.png -colors 1 txt: | awk 'NR==2 {print $3}')
+hexcode=$(magick $image -colors 1 txt: | awk 'NR==2 {print $3}')
 magick convert $image -filter point -resize 96x96 -bordercolor $bordercolor -border $border $icon
 notify-send -i $icon "  rang" "The hex code is $hexcode"
 wl-copy -n $hexcode
